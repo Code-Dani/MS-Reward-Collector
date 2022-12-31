bce82e2461517507986a54963a15c4e64a171bc7
Daniel <satrianodaniel@gmail.com>
2022-12-31 17:22:27 +0100

    Implemented other activities
    Made it so that the app also open form that are situated in the "other activities" panel.
    Increased the number of times "doSearches" do a query from 30 to 40 to secure the 90 points

--- javascript/mainPage.js
+++ javascript/mainPage.js
@@ -16,7 +16,6 @@ window.onload = function(){
     
     //Adding onclick event to the custom button
     document.getElementById("reward_pivot_collect").onclick = (function (){
-        console.log('entro');
         if(document.getElementsByClassName('m-card-group')[0].getElementsByClassName('ng-scope ng-isolate-scope c-card ').length == 3){
             console.log("Entered IF");
             //enters if the 3 daily challenges are loaded
@@ -24,11 +23,24 @@ window.onload = function(){
             document.getElementsByClassName('m-card-group')[0].getElementsByClassName('ng-scope ng-isolate-scope c-card ')[1].getElementsByTagName('a')[0].click();
             document.getElementsByClassName('m-card-group')[0].getElementsByClassName('ng-scope ng-isolate-scope c-card ')[2].getElementsByTagName('a')[0].click();
         }
+
+    
+        const container = document.getElementById("more-activities").children[0];
+        //check if "other activities" tab has anything in it (basically if it exists)
+        if(container.childElementCount > 0){
+            const numberOfItems = container.childElementCount; //usually 12
+            for(var i=0; i<numberOfItems;i++){  //cycle through all items
+                //make it so that it doesn't click the mobile app AD && if its already been collected (by checking if it has the typical green icon on the top-right corner)
+                if(!(container.children[i].hasAttribute("ng-class")) && container.children[i].getElementsByClassName("mee-icon mee-icon-SkypeCircleCheck").length == 0){    
+                    container.children[i].children[0].children[0].children[0].children[0].children[0].click();  //clicks the item
+                }
+            }
+        }
+        
     });
 
     //Adding onclick event to the custom button
     document.getElementById("reward_pivot_searches").onclick = (function (){
-        console.log('entro');
         window.open('https://www.bing.com/search?q=cwapchwjauhd1928745y6dwadaw231')
     });
 }
