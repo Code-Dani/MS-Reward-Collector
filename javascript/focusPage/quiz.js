7757c41ac5beba43f53acc8e10f33ac2377f7654
Daniel <satrianodaniel@gmail.com>
2023-02-20 22:21:25 +0100

    Started implementing 2itemquiz

--- javascript/focusPage/quiz.js
+++ javascript/focusPage/quiz.js
@@ -11,7 +11,15 @@ window.onload = function() {
 function checkQuizAvailable(){
     if(document.getElementById("QuizContainerWrapper") != null){
 
-        if (document.getElementsByClassName('rqECredits')[0].innerHTML === document.getElementsByClassName('rqMCredits')[0].innerHTML) {
+        const _8itemQuiz = document.getElementsByClassName('b_cards bt_lstcl_card btcc btcNoImg');
+        const _4itemQuiz = document.getElementsByClassName('rq_button');
+        const _2itemQuiz = document.getElementsByClassName('btOptions');
+
+        //const earnedCredits = document.getElementsByClassName('rqECredits')[0].innerHTML;
+        //const totalCredits = document.getElementsByClassName('rqMCredits')[0].innerHTML;
+        const greenCheckLogo = document.getElementsByClassName('cico rqSumryLogo'); //used to check if the quiz is completed
+
+        if (greenCheckLogo.length > 0) {
             //enters only if the quiz is already done: if current_amount_points = total_points_obtainable
             console.log("Closing page");
             window.close();
@@ -19,15 +27,22 @@ function checkQuizAvailable(){
 
         //In both of these case there is no need to press the "start quiz" button when prompted on the page.
         //It is also possible to make a condition to not click already clicked items, but there is no gaining on doing that, so I'm not going to.
-        const _8itemQuiz = document.getElementsByClassName('b_cards bt_lstcl_card btcc btcNoImg');
-        const _4itemQuiz = document.getElementsByClassName('rq_button');
-        if(_8itemQuiz.length > 0){
+        if(_8itemQuiz.length > 0)
             do8QuizActivity(_8itemQuiz);
-        } else{
+        if(_4itemQuiz.length > 0)
             do4QuizActivity(_4itemQuiz);
-        }
-
+        if(_2itemQuiz.length > 0)
+            do2QuizActivity(_2itemQuiz[0]);
     }
+}
+
+/**
+ * Automatically does the 2item quiz
+ * @since 20/02/2023
+ * @param elements Array of element to cycle through
+ */
+function do2QuizActivity(elements){
+    elements.children[0].click();
 }
 
 /**
